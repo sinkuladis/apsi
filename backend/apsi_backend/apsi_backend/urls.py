8e979c5d33d38e77153ce83d71ab9e9330eb0de9
Piotr Roszkowski <przkladowy.mail>
2020-04-21 18:11:18 +0200

    created backend_apsi app, added basic logging

--- backend/apsi_backend/apsi_backend/urls.py
+++ backend/apsi_backend/apsi_backend/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url('', include('rest_framework.urls'))
 ]
