a569fb28e20f29e13d9b5156d5fa9ff9d14b7102
m-zawilski <mzawilski1@gmail.com>
2020-04-26 18:47:48 +0200

    Added Highlights

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="main-body">
         <categories-picker/>
-        <highlights-list/>
+        <highlights-list 
+            title="Ogłoszenia promowane"
+            :items=promotedItems
+        />
+        <highlights-list 
+            title="Ostatnio dodane"
+            :items=lastAdded
+        />
     </div>
 </template>
 
@@ -10,7 +17,21 @@
     import HighlightsList from "@/components/HighlightsList";
     export default {
         name: 'Home',
-        components: {HighlightsList, CategoriesPicker}
+        components: { HighlightsList, CategoriesPicker },
+        data: () => {
+            return { 
+                    promotedItems: [
+                        { title: 'Promowany 1', price: "3zł"}, 
+                        { title: 'Promowany 2', price: "300zł"}, 
+                        { title: 'Promowany 3', price: "123zł"}
+                    ],
+                    lastAdded: [
+                        { title: 'Ostatnio dodany 1', price: "54zł"}, 
+                        { title: 'Ostatnio dodany 2', price: "124zł"}, 
+                        { title: 'Ostatnio dodany 3', price: "500zł"}
+                    ],
+                }
+        }
     }
 </script>
 
