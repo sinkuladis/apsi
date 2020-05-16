6fcb5b5efefb5bcb5281a60d3e03bd627103bed5
usinkevi <sink.vlad@gmail.com>
2020-05-16 17:54:52 +0200

    Code formating. Removing unused imports

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,11 +9,11 @@
         </section>
         <section>
             <!--White area section-->
-            <highlights-list 
+            <highlights-list
                 title="Ogłoszenia promowane"
                 :items=promotedItems
             />
-            <highlights-list 
+            <highlights-list
                 title="Ostatnio dodane"
                 :items=lastAdded
             />
@@ -29,29 +29,29 @@
         name: 'Home',
         components: { HighlightsList, CategoriesPicker, SearchSection},
         data: () => {
-            return { 
+            return {
                     promotedItems: [
-                        { title: 'Promowany 1', price: "3zł"}, 
-                        { title: 'Promowany 2', price: "300zł"}, 
+                        { title: 'Promowany 1', price: "3zł"},
+                        { title: 'Promowany 2', price: "300zł"},
                         { title: 'Promowany 3', price: "123zł"}
                     ],
                     lastAdded: [
-                        { title: 'Ostatnio dodany 1', price: "54zł"}, 
-                        { title: 'Ostatnio dodany 2', price: "124zł"}, 
+                        { title: 'Ostatnio dodany 1', price: "54zł"},
+                        { title: 'Ostatnio dodany 2', price: "124zł"},
                         { title: 'Ostatnio dodany 3', price: "500zł"}
                     ],
                     categories: [
-                        { icon: 'mdi-car', name: 'Motoryzacja' }, 
-                        { icon: 'mdi-home', name: 'Nieruchomości' }, 
-                        { icon: 'mdi-worker', name: 'Praca' }, 
-                        { icon: 'mdi-sofa', name: 'Dom i ogród' }, 
-                        { icon: 'mdi-laptop', name: 'Elektronika' }, 
-                        { icon: 'mdi-tshirt-crew', name: 'Moda' }, 
-                        { icon: 'mdi-dog-side', name: 'Zwierzęta' }, 
-                        { icon: 'mdi-human-child', name: 'Dla dzieci' }, 
-                        { icon: 'mdi-basketball', name: 'Sport' }, 
-                        { icon: 'mdi-book-open', name: 'Edukacja' }, 
-                        { icon: 'mdi-guitar-acoustic', name: 'Muzyka' }, 
+                        { icon: 'mdi-car', name: 'Motoryzacja' },
+                        { icon: 'mdi-home', name: 'Nieruchomości' },
+                        { icon: 'mdi-worker', name: 'Praca' },
+                        { icon: 'mdi-sofa', name: 'Dom i ogród' },
+                        { icon: 'mdi-laptop', name: 'Elektronika' },
+                        { icon: 'mdi-tshirt-crew', name: 'Moda' },
+                        { icon: 'mdi-dog-side', name: 'Zwierzęta' },
+                        { icon: 'mdi-human-child', name: 'Dla dzieci' },
+                        { icon: 'mdi-basketball', name: 'Sport' },
+                        { icon: 'mdi-book-open', name: 'Edukacja' },
+                        { icon: 'mdi-guitar-acoustic', name: 'Muzyka' },
                         { icon: 'mdi-postage-stamp', name: 'Hobby' }
                     ]
                 }
