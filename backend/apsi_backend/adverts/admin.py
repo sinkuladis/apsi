39636b5c10e5b783203b8bbebb8fbf23b3e0fa58
Kuba <kubag0125@gmail.com>
2020-05-19 21:18:57 +0200

    Backend: Delete apps("adverts", "users")

--- backend/apsi_backend/adverts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Adverts, Advert_Status, Advert_Promotions
-
-# Register your models here.
-admin.site.register(Adverts)
-admin.site.register(Advert_Promotions)
-admin.site.register(Advert_Status)
