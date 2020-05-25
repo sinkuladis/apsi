24a1c554f2fc8c965f4deaf5d57310b45d516157
Ewelina Bartuzi <eb1993@vp.pl>
2020-05-25 20:46:23 +0200

    verbose name plural

--- backend/apsi_backend/apsi_app/models.py
+++ backend/apsi_backend/apsi_app/models.py
@@ -15,6 +15,9 @@ class Country(models.Model):
     def __str__(self):
         return '%s' % (self.name)
 
+    class Meta:
+        verbose_name_plural = "Counties"
+
 
 class Province(models.Model):
     name = models.CharField(max_length=100)
@@ -31,6 +34,9 @@ class City(models.Model):
     def __str__(self):
         return '%s' % (self.name)
 
+    class Meta:
+        verbose_name_plural = "Cities"
+
 
 class User(AbstractUser):
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
@@ -42,6 +48,9 @@ class AdvertCategory(models.Model):
 
     def __str__(self):
         return '%s' % (self.name)
+
+    class Meta:
+        verbose_name_plural = "AdvertCategories"
 
 
 class AdvertPromotion(models.Model):
@@ -58,20 +67,25 @@ class AdvertStatus(models.Model):
     def __str__(self):
         return '%s' % (self.name)
 
+    class Meta:
+        verbose_name_plural = "AdvertStatus"
+
 
 class Advert(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=9000)
     price = models.FloatField(validators=[MinValueValidator(0.01), MaxValueValidator(1000000)])
     for_free = models.BooleanField(default=False)
-    advert_category = models.ForeignKey(AdvertCategory, on_delete=models.CASCADE, related_name='category_name', null=True, blank=True)
+    advert_category = models.ForeignKey(AdvertCategory, on_delete=models.CASCADE, related_name='category_name',
+                                        null=True, blank=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='city_name', null=True, blank=True)
     create_date = models.DateField(default=datetime.date.today)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name', null=True, blank=True)
-    promotion = models.ForeignKey(AdvertPromotion, on_delete=models.CASCADE, related_name='promotion_name', null=True, blank=True)
-    advert_status = models.ForeignKey(AdvertStatus, on_delete=models.CASCADE, related_name='advert_status_name', null=True, blank=True)
+    promotion = models.ForeignKey(AdvertPromotion, on_delete=models.CASCADE, related_name='promotion_name', null=True,
+                                  blank=True)
+    advert_status = models.ForeignKey(AdvertStatus, on_delete=models.CASCADE, related_name='advert_status_name',
+                                      null=True, blank=True)
     subscribing_users = models.ManyToManyField('User', 'subscribing_users')
-    
 
     def __str__(self):
         return '%s' % (self.title)
@@ -90,9 +104,16 @@ class ObservedAds(models.Model):
     advert_id = models.ForeignKey(Advert, on_delete=models.CASCADE)
     # quantity = models.PositiveSmallIntegerField(default=1)#IntegerField()
     # product_id = models.PositiveSmallIntegerField(default=1) # i dont't understand this field
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # added, did not exist in the database model
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)  # added, did not exist in the database model
+
+    class Meta:
+        verbose_name_plural = "ObservedAds"
+
 
 class AdvertItems(models.Model):
     advert_id = models.ForeignKey(Advert, on_delete=models.CASCADE)
     # quantity = models.PositiveSmallIntegerField(default=1)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "AdvertItems"
