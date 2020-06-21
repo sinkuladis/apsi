5d25e47f0a666ddf576141c338dcdb6380598232
pwalkowiak <pwalkowiak>
2020-06-21 14:54:30 +0200

    Imports hotfix

--- backend/apsi_backend/apsi_app/admin.py
+++ backend/apsi_backend/apsi_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import City, Country, Province, Advert, AdvertCategory, AdvertStatus, AdvertMessage, AdvertPromotion, AdvertItems
+from .models import City, Country, Province, Advert, AdvertCategory, AdvertStatus, AdvertPromotion, AdvertItems
 
 # Register your models here.
 admin.site.register(City)
@@ -7,7 +7,4 @@ admin.site.register(Country)
 admin.site.register(Province)
 admin.site.register(Advert)
 admin.site.register(AdvertCategory)
-#admin.site.register(AdvertStatus)
-#admin.site.register(AdvertMessage)
-#admin.site.register(AdvertPromotion)
 admin.site.register(AdvertItems)
